e4d5e8e2ed262864eab3d1d0a774d8356524ad19
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-12-31 19:40:39 +0500

    [Feat] Rewrote duplicator set_ttl to raw sockets TTL

--- duplicate/mod.rs
+++ duplicate/mod.rs
@@ -9,12 +9,52 @@ use std::net::TcpStream;
 use std::io::Write;
 use crate::core;
 
+pub fn set_ttl_raw(mut socket: &TcpStream, ttl: u8) {
+  #[cfg(target_os = "linux")]
+  if cfg!(unix) {
+    #[cfg(target_os = "linux")]
+    use libc::{setsockopt, IP_TTL, IPPROTO_IP};
+    #[cfg(target_os = "linux")]
+    use std::os::unix::io::{AsRawFd, RawFd};
+
+    #[cfg(target_os = "linux")]
+    let fd = socket.as_raw_fd();
+
+    #[cfg(target_os = "linux")]
+    let _ = unsafe { 
+      #[cfg(target_os = "linux")]
+      setsockopt(fd, IPPROTO_IP, IP_TTL, &ttl as *const _ as *const libc::c_void, std::mem::size_of::<c_int>() as c_int);
+    };
+  } else if cfg!(windows) {
+    #[cfg(target_os = "windows")]
+    use winapi::um::winsock2::{setsockopt, IP_TTL, IPPROTO_IP, SOCKET, SOCK_STREAM};
+    #[cfg(target_os = "windows")]
+    use std::os::windows::io::{AsRawSocket, RawSocket};
+    #[cfg(target_os = "windows")]
+    use std::ptr;
+
+    #[cfg(target_os = "windows")]
+    let rs: RawSocket = socket.as_raw_socket();
+
+    #[cfg(target_os = "windows")]
+    let result = unsafe {
+      setsockopt(
+        rs as SOCKET,
+        IPPROTO_IP,
+        IP_TTL,
+        &ttl as *const i32 as *const _,
+        std::mem::size_of::<i32>() as i32
+      )
+    };
+  }
+}
+
 pub fn send(mut socket: &TcpStream, packet: Vec<u8>) -> Result<(), std::io::Error> {
   let conf: core::AuxConfig = core::parse_args();
 
-  socket.set_ttl(conf.disorder_packet_ttl.into())?;
+  set_ttl_raw(&socket, conf.disorder_packet_ttl.into());
   socket.write_all(&packet.as_slice())?;
-  socket.set_ttl(conf.default_ttl.into())?;
+  set_ttl_raw(&socket, conf.default_ttl.into());
 
   Ok(())
 }
