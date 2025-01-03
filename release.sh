920a51f49ad1562103146045ff04595d6c6cb0c2
0xffabc <139798156+0xffabc@users.noreply.github.com>
2025-01-03 19:26:59 +0500

    [Ref] Minor changes to fix building

--- release.sh
+++ release.sh
@@ -1,1 +1,6 @@
-cargo build --all-targets --config "linker='rust-lld'"
+cargo build --target arm-linux-androideabi --release
+cargo build --target x86_64-unknown-linux-gnu --release
+cargo build --target x86_64-unknown-linux-musl --release
+cargo build --target armv7-linux-androideabi --release
+cargo build --target x86_64-pc-windows-gnu --release
+cargo build --release
