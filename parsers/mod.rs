d3c87ff90b7d7eb7dec952a8dcbf09e8fcfa0cb2
Pulsar <139798156+pulsarf@users.noreply.github.com>
2025-04-22 19:43:26 +0500

    [Fix] IP Parsing and DNS

--- parsers/mod.rs
+++ parsers/mod.rs
@@ -13,93 +13,127 @@ pub mod parsers {
 
   impl IpParser {
     pub fn parse(buffer: Vec<u8>) -> IpParser {
-      let dest_addr_type: u8 = buffer[3];
+      if buffer.len() < 10 {
+        return IpParser {
+          dest_addr_type: 0,
+          host_raw: vec![],
+          host_unprocessed: vec![],
+          port: 0,
+          is_udp: false
+        };
+      }
 
+      let dest_addr_type = buffer[3];
       let is_udp = buffer[1] == 0x03;
 
       match dest_addr_type {
         1 => {
+          if buffer.len() < 10 {
+            return IpParser {
+              dest_addr_type,
+              host_raw: vec![0; 4],
+              host_unprocessed: vec![0; 4],
+              port: 0,
+              is_udp
+            };
+          }
           IpParser {
             dest_addr_type,
-            host_raw: vec![buffer[4], buffer[5], buffer[6], buffer[7]],
-            host_unprocessed: vec![buffer[4], buffer[5], buffer[6], buffer[7]],
+            host_raw: buffer[4..8].to_vec(),
+            host_unprocessed: buffer[4..8].to_vec(),
             port: u16::from_be_bytes([buffer[8], buffer[9]]),
             is_udp
           }
         },
         3 => {
-          let domain_length = buffer[4] as usize;
-          let domain = &buffer[5..5 + domain_length];
-          let domain_str = std::str::from_utf8(domain).unwrap().to_string();
-
-          if domain.len() < 4 {
-              return IpParser {
-                  dest_addr_type, host_raw: vec![0, 0, 0, 0],
-                  host_unprocessed: domain.to_vec(),
-                  port: 443,
-                  is_udp
-              };
+          if buffer.len() < 5 {
+            return IpParser {
+              dest_addr_type,
+              host_raw: vec![0; 4],
+              host_unprocessed: vec![],
+              port: 0,
+              is_udp
+            };
           }
 
-          let domain_slice = domain_str.to_socket_addrs();
+          let domain_length = buffer[4] as usize;
+          if buffer.len() < 5 + domain_length + 2 {
+            return IpParser {
+              dest_addr_type,
+              host_raw: vec![0; 4],
+              host_unprocessed: vec![],
+              port: 0,
+              is_udp
+            };
+          }
 
-          let domain_str_at = domain_str.parse::<IpAddr>();
+          let domain = &buffer[5..5 + domain_length];
+          let port = u16::from_be_bytes([buffer[5 + domain_length], buffer[6 + domain_length]]);
 
-          if domain_str_at.is_ok() {
-              let ip_buffer = match domain_str_at.unwrap() {
+          if let Ok(domain_str) = std::str::from_utf8(domain) {
+            if let Ok(ip_addr) = domain_str.parse::<IpAddr>() {
+              let ip_buffer = match ip_addr {
+                IpAddr::V4(ip) => ip.octets().to_vec(),
+                IpAddr::V6(ip) => ip.octets().to_vec(),
+              };
+              return IpParser {
+                dest_addr_type,
+                host_raw: ip_buffer,
+                host_unprocessed: domain.to_vec(),
+                port,
+                is_udp
+              };
+            }
+
+            if let Ok(ip) = doh_resolver(domain_str.to_string()) {
+              if let Ok(ip_addr) = ip.parse::<IpAddr>() {
+                let ip_buffer = match ip_addr {
                   IpAddr::V4(ip) => ip.octets().to_vec(),
                   IpAddr::V6(ip) => ip.octets().to_vec(),
-              };
-
-              return IpParser {
+                };
+                return IpParser {
                   dest_addr_type,
                   host_raw: ip_buffer,
                   host_unprocessed: domain.to_vec(),
-                  port: 443,
+                  port,
                   is_udp
+                };
               }
+            }
           }
 
-          match doh_resolver(domain_str.clone()) {
-              Ok(ip) => {
-                  if let Ok(ip_addr) = ip.parse::<IpAddr>() {
-                      let ip_buffer = match ip_addr {
-                          IpAddr::V4(ip) => ip.octets().to_vec(),
-                          IpAddr::V6(ip) => ip.octets().to_vec(),
-                      };
-
-                      return IpParser {
-                          dest_addr_type,
-                          host_raw: ip_buffer,
-                          host_unprocessed: domain.to_vec(),
-                          port: 443,
-                          is_udp
-                      };
-                  } else {
-                      return IpParser {
-                          dest_addr_type, host_raw: vec![0, 0, 0, 0],
-                          host_unprocessed: domain.to_vec(),
-                          port: 443,
-                          is_udp
-                      };
-                  };
-              }
-              Err(e) => {
-                  return IpParser {
-                      dest_addr_type, host_raw: vec![0, 0, 0, 0],
-                      host_unprocessed: domain.to_vec(),
-                      port: u16::from_be_bytes([buffer[8], buffer[9]]),
-                      is_udp
-                  }
-              }
+          IpParser {
+            dest_addr_type,
+            host_raw: vec![0; 4],
+            host_unprocessed: domain.to_vec(),
+            port,
+            is_udp
           }
         },
-        _ => {
+        4 => {
+          if buffer.len() < 22 {
+            return IpParser {
+              dest_addr_type,
+              host_raw: vec![0; 16],
+              host_unprocessed: vec![0; 16],
+              port: 0,
+              is_udp
+            };
+          }
           IpParser {
             dest_addr_type,
             host_raw: buffer[4..20].to_vec(),
             host_unprocessed: buffer[4..20].to_vec(),
             port: u16::from_be_bytes([buffer[20], buffer[21]]),
+            is_udp
+          }
+        },
+        _ => {
+          IpParser {
+            dest_addr_type,
+            host_raw: vec![],
+            host_unprocessed: vec![],
+            port: 0,
             is_udp
           }
         }
