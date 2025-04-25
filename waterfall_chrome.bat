c1e845cd20b0129e761761989e8d3c2212f48af7
Pulsar <139798156+pulsarf@users.noreply.github.com>
2025-04-25 19:06:16 +0500

    [F] Update config for extreme case

--- waterfall_chrome.bat
+++ waterfall_chrome.bat
@@ -1,3 +1,7 @@
-start "" target/release/waterfall.exe --bind_port 10000 ^
---fake_as_oob --fake_packet_ttl 2 --fake_packet_sni gosuslugi.ru --fake_packet_random ^
---send_fake_clienthello
+start "" target/release/waterfall --bind_port 10000 --packet_hop 2 ^
+--disable_sack --send_fake_clienthello --fc_sni yandex.ru --fake_packet_random ^
+--fake_packet_override_data .gosuslugi.ru --oob_stream_hell_data .yandex.ru ^
+--fake_packet_ttl 8 --disorder_packet_ttl 1 ^
+--tcp_out_of_band_hell -3+s --tcp_fake_insert 3+s --tcp_out_of_band_hell 6+s ^
+--fake_packet_send_http --fake_packet_host yandex.ru ^
+--http_host_cmix --http_host_rmspace
