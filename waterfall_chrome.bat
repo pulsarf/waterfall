30041db0601d7e006290feef983031f743cfcc76
Pulsar <139798156+pulsarf@users.noreply.github.com>
2025-04-26 13:19:53 +0500

    [F] Bump config

--- waterfall_chrome.bat
+++ waterfall_chrome.bat
@@ -4,4 +4,5 @@ start "" target/release/waterfall --bind_port 10000 --packet_hop 2 ^
 --fake_packet_ttl 8 --disorder_packet_ttl 1 ^
 --tcp_out_of_band_hell -3+s --tcp_fake_insert 3+s --tcp_out_of_band_hell 6+s ^
 --fake_packet_send_http --fake_packet_host yandex.ru ^
---http_host_cmix --http_host_rmspace
+--http_host_cmix --http_host_rmspace ^
+--resist_timing_attack 15 --so_recv_size 3000 --so_send_size 16654 --so_opt_cutoff 30
